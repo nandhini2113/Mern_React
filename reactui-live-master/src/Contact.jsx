3286f1f12b0884e22fdce02c7b33bde4b80edac5
nandhini2113 <moodinandini@gmail.com>
2024-10-28 20:52:00 +0530

    added few changes

--- reactui-live-master/src/Contact.jsx
+++ reactui-live-master/src/Contact.jsx
@@ -15,8 +15,10 @@ function Contact() {
                         name="access_key"
                         value="0480846b-a030-4aa1-98c1-f705eaedddf8"
                     />
+                    <div className='left-title'>
                     <h2>Contact Us</h2>
                     <hr />
+                    </div>
                     <input type="text" name='text' placeholder='Enter Name' required /><br />
                     <input type="email" name='email' placeholder='Enter Email' required /><br />
                     <textarea name='message' placeholder='Enter Message'></textarea>
